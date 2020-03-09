c5afa1628f0a2c3c96819715fe5c6139eee6debf
taron-jamal <61537513+taron-jamal@users.noreply.github.com>
2020-03-09 04:38:26 -0700

    Add files via upload

---
+++ workProfile.js
@@ -0,0 +1,22 @@
+// Declare the function 'workProfile'.
+  function workProfile() {
+    theOject.role = "Web Developer I";
+  }
+
+
+// Declare variable 'myProfile'.
+  var myProfile = {
+    name: 'TaRon Jamal',
+    department: 'Research and Development',
+    hireDate: 2020
+  };
+
+
+// Logs 'TaRon Jamal'
+console.log(myProfile.name);
+
+// Pass object reference to the function.
+workProfile(myProfile);
+
+// Log's 'Toyota' as the value of the 'name' property of the object, as changed to by the function.
+console.log(myProfile.name);
